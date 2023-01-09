bdb303d84182c5d136dd8171fc59bd399ff930bc
Saori Isabel Tovar Niño <39502938+saorionline@users.noreply.github.com>
2023-01-09 14:59:50 -0500

    most advances I did Slider almost there

---
+++ slider.js
@@ -0,0 +1,3 @@
+const card = document.querySelector(".main-announce-1");
+const myAncherSlider = document.querySelector(".arrow");
+const myAncheRight = document.querySelector(".arrow-point-right");
