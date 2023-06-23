aca9735fa2c60edde79083026352b401f0a8c7d4
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2023-06-22 19:17:38 -0500

    hasta aqui para continuar con Vercel

--- React-Brewery/src/index.js
+++ React-Brewery/src/index.js
@@ -1,1 +1,6 @@
-console.log('Stars are blind')
+//console.log('Stars are blind')
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Love is serious</h1>, document.getElementById('app'))
--- React-Brewery/webpack.config.js
+++ React-Brewery/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { Template } = require("webpack")
 
 module.exports = {
         mode: 'development',
@@ -6,6 +7,25 @@ module.exports = {
                 filename: 'app.bundle.js'
         },
         plugins: [
-                new HtmlWebpackPlugin()
-        ]
+                new HtmlWebpackPlugin({
+                        template: 'src/index.html'
+                })
+        ],
+        module: {
+                rules: [
+                        {
+                                test: /\.js$/,
+                                exclude: /node_modules/,
+                                use: {
+                                        loader: 'babel-loader',
+                                        options: {
+                                                presets: [
+                                                        '@babel/preset-env',
+                                                        '@babel/preset-react'
+                                                ]
+                                        }
+                                }
+                        }
+                ]
+        }
 }
